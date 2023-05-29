856e4f874b07a78e2aa308220600c4c000908eca
Oleksandr Fedorov <freezers@ukr.net>
2023-05-29 22:47:27 +0300

    Asyncronous emails sending and btc-uah rate fetching

--- fetchbtcrate/fetch_rate_main.go
+++ fetchbtcrate/fetch_rate_main.go
@@ -2,24 +2,41 @@ package fetchbtcrate
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/namefreezers/genesis-ses-assignment/fetchbtcrate/coinbase"
 	"github.com/namefreezers/genesis-ses-assignment/fetchbtcrate/coingecko"
 )
 
+type btcUahFetcherFunc func() (float64, error)
+
+func fetchBtcUahAsync(fetcher btcUahFetcherFunc, ch chan<- float64) {
+	rate, err := fetcher()
+	if err == nil {
+		ch <- rate
+	}
+}
+
 // Try to fetch btc-uah rate from few third-party api's one-by-one synchronously
-// (There is an option for enhancement, to make request asyncronously, 
+// (There is an option for enhancement, to make request asyncronously,
 // and return response from first answered third-party api.)
 func FetchBtcUahRateMain() (float64, error) {
-	rate, err := coinbase.FetchBtcUahRate()
-	if err == nil {
-		return rate, nil
-	}
 
-	rate, err = coingecko.FetchBtcUahPrice()
-	if err == nil {
-		return rate, nil
-	}
+	ch := make(chan float64)
 
-	return 0, fmt.Errorf("All btc rate services are unavailable. Last fetch error: %v", err.Error())
+	// run few btc-uah rate fetches asyncronously
+	go fetchBtcUahAsync(coinbase.FetchBtcUahRate, ch)
+	go fetchBtcUahAsync(coingecko.FetchBtcUahPrice, ch)
+
+	// 10 second timeout
+	timeout := time.After(10 * time.Second)
+	select {
+	case rate := <-ch:
+		// return first responded service's answer
+		return rate, nil
+	case <-timeout:
+		// return error, if we reached the timeout
+		fmt.Println("timed out all requests")
+		return 0, fmt.Errorf("all btc rate services are unavailable")
+	}
 }
--- sendemail/sendemail.go
+++ sendemail/sendemail.go
@@ -4,28 +4,44 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"net/smtp"
 )
 
-// emails set might be big, and frequency of api calls also might be big, 
+func tryToSendEmail(email_to string, btc_uah_price float64, auth smtp.Auth, wg *sync.WaitGroup) {
+	if wg != nil {
+		// we send emails asyncronously, so `WaitGroup` is needed to wait for all email-sending-goroutines
+		defer wg.Done()
+	}
+
+	msg := []byte(fmt.Sprintf("Subject: Bitcoin price\r\n"+
+		"From: O. Fedorov's Software Engineering School Assignment <o.fedorov.genesis.assignment@gmail.com>\r\n"+
+		"To: %v\r\n"+
+		"Hi! Current BTC-UAH price is %.2f!\r\n", email_to, btc_uah_price))
+
+	err := smtp.SendMail("smtp.gmail.com:587", auth, os.Getenv("EMAIL_USERNAME"), []string{email_to}, msg)
+
+	// we don't need to response an error from api if we couldn't send mail, so we drop this error
+	if err != nil {
+		log.Printf("Unable to send email to: %v. Reason: %v", email_to, err.Error())
+	}
+}
+
+// emails set might be big, and frequency of api calls also might be big,
 // so there is better to pass it by pointer
 func TryToSendEmailsBtcUahPrice(emails_set_to_send *map[string]struct{}, btc_uah_price float64) {
 
 	auth := smtp.PlainAuth("", os.Getenv("EMAIL_USERNAME"), os.Getenv("EMAIL_PASSWORD"), "smtp.gmail.com")
 
-	// send emails in a loop to have possibility to set header `To:` for each recipient individually.
-	// (There is an option for enhancement to send all these emails asyncronously.)
-	for email := range *emails_set_to_send {
-		msg := []byte(fmt.Sprintf("Subject: Bitcoin price\r\n"+
-			"From: O. Fedorov's Software Engineering School Assignment <o.fedorov.genesis.assignment@gmail.com>\r\n"+
-			"To: %v\r\n"+
-			"Hi! Current BTC-UAH price is %.2f!\r\n", email, btc_uah_price))
+	// We will send them asyncronously to speed-up all the mailing. So we will wait for each goroutine to finish
+	var wg sync.WaitGroup
+	wg.Add(len(*emails_set_to_send))
 
-		err := smtp.SendMail("smtp.gmail.com:587", auth, os.Getenv("EMAIL_USERNAME"), []string{email}, msg)
-
-		if err != nil {
-			log.Printf("Unable to send email to: %v. Reason: %v", email, err.Error())
-		}
+	// send emails separately to have possibility to set header `To:` for each recipient individually.
+	for email_to_send := range *emails_set_to_send {
+		go tryToSendEmail(email_to_send, btc_uah_price, auth, &wg)
 	}
+
+	wg.Wait()
 }
